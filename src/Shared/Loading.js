8c4597d4b788a40021fe054911258cb637dfebd7
unknown <mdtanvir6070@gmail.com>
2022-10-07 11:22:30 +0600

    Folder stracture and npm packages added

---
+++ src/Shared/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SpinnerDiamond } from "spinners-react";
+
+const Loading = () => {
+  return (
+    <div>
+      <SpinnerDiamond size="100%" сolor="#38ad48" enabled={false} />
+    </div>
+  );
+};
+
+export default Loading;
